de03b2a2f585e17b0d27ce4e9cfb811073e20118
steelpwner <molinaestren@gmail.com>
2021-05-28 12:54:11 -0500

    added some verifications

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ validateUserInput = (req, res, next) => {
     if (!validatePhone(req.body.phone)) {
         return res.status(200).json({"status":422,"message":"Hubo un error al revisar el teléfono, por favor verifíquelo."})
     }
+    if (req.body.user.contains(" ")) {
+        return res.status(200).json({"status":422,"message":"El nombre de usuario no puede contener espacios."})
+    }
+    if (req.body.pass.contains(" ")) {
+        return res.status(200).json({"status":422,"message":"La contraseña no puede contener espacios."})
+    }
     next()
 }
 
