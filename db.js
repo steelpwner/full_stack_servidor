a6a31ed0bd7dc751582f5bffed7d81041bd1db14
steelpwner <molinaestren@gmail.com>
2021-05-28 00:13:33 -0500

    news module added

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ const sqlite3 = require('sqlite3')
 var db = new sqlite3.Database('./data.db')
 
 db.serialize(function() {
-
     db.run(`CREATE TABLE usuario (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 nombre TEXT NOT NULL,
@@ -15,7 +14,7 @@ db.serialize(function() {
             CREATE UNIQUE INDEX usuario_unique_email ON usuario(correo);
             CREATE UNIQUE INDEX usuario_unique_usuario ON usuario(usuario);
             `)
-
+    db.run("DROP TABLE noticia")
     db.run(`CREATE TABLE noticia (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nombre TEXT NOT NULL,
@@ -25,8 +24,7 @@ db.serialize(function() {
             categoria TEXT NOT NULL,
             imagen TEXT NOT NULL,
             resumen TEXT NOT NULL,
-            fecha_creacion DATETIME NOT NULL,
-            fecha_actualizacion DATETIME NOT NULL,
+            fecha_creacion DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
             usuario BIGINT NOT NULL,
             FOREIGN KEY (usuario)
                 REFERENCES usuario(id)
--- index.js
+++ index.js
@@ -50,11 +50,44 @@ validateUserInput = (req, res, next) => {
     names = req.body
     for (i = 0; i < params.length; i++) {
         if (!Object.keys(names).includes(params[i])) {
-            return res.status(200).json({"status":422,"message":"Faltaron parámetros para la función"})
+            return res.status(200).json({"status":422,"message":"Faltaron parámetros para la creación"})
         }
     }
     if (!validateEmail(req.body.email) || !validatePhone(req.body.phone)) {
         return res.status(200).json({"status":422,"message":"Hubo un error al revisar el email o el teléfono, por favor verifíquelos."})
+    }
+    next()
+}
+
+validateNewsInput = (req, res, next) => {
+    params = ["name","description","newsman","user","visible","category","image","resume","user"]
+    names = req.body
+    for (i = 0; i < params.length; i++) {
+        if (!Object.keys(names).includes(params[i])) {
+            return res.status(200).json({"status":422,"message":"Faltaron parámetros para la creación"})
+        }
+    }
+    next()
+}
+
+validateNewsEdit = (req, res, next) => {
+    params = ["id", "name","description","newsman","visible","category","image","resume"]
+    names = req.body
+    for (i = 0; i < params.length; i++) {
+        if (!Object.keys(names).includes(params[i])) {
+            return res.status(200).json({"status":422,"message":"Faltaron parámetros para la edición"})
+        }
+    }
+    next()
+}
+
+validateNewsRemove = (req, res, next) => {
+    params = ["id"]
+    names = req.body
+    for (i = 0; i < params.length; i++) {
+        if (!Object.keys(names).includes(params[i])) {
+            return res.status(200).json({"status":422,"message":"Faltaron parámetros para la edición"})
+        }
     }
     next()
 }
@@ -64,7 +97,7 @@ generateAccessToken = (username) => {
 }
 
 app.post("/login", (req, res) => {
-  let sql = `SELECT nombre, direccion, telefono, usuario, correo FROM usuario WHERE usuario = "${req.body.user}" AND pass = "${crypto.createHmac('sha256', process.env.TOKEN_SECRET).update(req.body.pass).digest('hex')}";`
+  let sql = `SELECT * FROM usuario WHERE usuario = "${req.body.user}" AND pass = "${crypto.createHmac('sha256', process.env.TOKEN_SECRET).update(req.body.pass).digest('hex')}";`
   db.get(sql, [], (err,user) => {
   if (err) {
     res.status(400).json({error:err.message})
@@ -100,8 +133,54 @@ app.post("/users/create", validateUserInput, (req, res) => {
 
 
 app.get("/news", (req, res) => {
-    sql = "SELECT nombre, descripcion, periodista, visible, categoria, imagen, resumen, fecha_creacion FROM noticia WHERE visible = 1;"
-    
+    sql = "SELECT * FROM noticia WHERE visible = 1;"
+    db.all(sql,{},(err, rows) => {
+        if (err) {
+            res.status(200).json({"status":400,"error":err.message})
+        }
+        return res.status(200).json({"status":200,data:rows})
+    })
+})
+
+/*
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            descripcion TEXT NOT NULL,
+            periodista TEXT NOT NULL,
+            visible TINYINT NOT NULL DEFAULT 1,
+            categoria TEXT NOT NULL,
+            imagen TEXT NOT NULL,
+            resumen TEXT NOT NULL,
+            fecha_creacion DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            usuario BIGINT NOT NULL, */
+
+app.post("/news/create", authenticateToken, validateNewsInput, (req,res) => {
+    params = ["name","description","newsman","user","visible","category","image","resume","user"]
+    sql = `INSERT INTO noticia(nombre,descripcion,periodista,visible,categoria,imagen,resumen,usuario) 
+           VALUES 
+           ('${req.body['name']}','${req.body['description']}','${req.body['newsman']}',
+           '${req.body['visible']}','${req.body['category']}','${req.body['image']}',
+           '${req.body['resume']}','${req.body['user']}')`
+    db.run(sql);
+    return res.status(200).json({"status":200,"message":"Noticia creada correctamente"}) 
+})
+
+app.delete("/news", authenticateToken, validateNewsRemove, (req,res) => {
+    sql = `DELETE FROM noticia WHERE id = '${req.body['id']}'`
+    console.log(sql)
+    db.run(sql);
+    return res.status(200).json({"status":200,"message":"Noticia eliminada correctamente"}) 
+})
+
+app.put("/news", authenticateToken, validateNewsEdit, (req,res) => {
+    sql = `UPDATE NOTICIA
+           SET nombre='${req.body['name']}', descripcion='${req.body['description']}',
+           periodista='${req.body['newsman']}', visible='${req.body['visible']}',
+           categoria='${req.body['category']}', imagen='${req.body['image']}',
+           resumen='${req.body['resume']}'
+           WHERE id='${req.body['id']}'`
+           db.run(sql);
+           return res.status(200).json({"status":200,"message":"Noticia editada correctamente"}) 
 })
 
 app.get("/users/self", authenticateToken, (req, res) => {
